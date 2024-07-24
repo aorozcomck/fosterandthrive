fd3a1a879c3fe2178a5b811960749a4ba7a6a5fd
Andrea Orozco Sanchez <andrea.orozco@yuxiglobal.com>
2024-07-24 16:57:58 -0500

    First configuration

---
+++ src/app/why-ft/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+// import WhyFTComponent from "@/components/why_FT/WhyFTComponent";
+
+
+function FTComponent() {
+
+
+  return (
+    <>
+      <div className="wrapper">
+        {/* <WhyFTComponent isCarusolAvaibleProps={setisCarusonAvible} /> */}
+      </div>
+    </>
+  );
+}
+export default FTComponent;
